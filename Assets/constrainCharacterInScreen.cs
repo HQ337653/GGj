b035803da04969b01aa84264a79510f7e9a1892a
HQ337653 <95904998+HQ337653@users.noreply.github.com>
2023-02-05 11:11:05 +0800

    constrain player based on camera

--- Assets/constrainCharacterInScreen.cs
+++ Assets/constrainCharacterInScreen.cs
@@ -1,18 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
+using UnityEngine;
 public class constrainCharacterInScreen : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject[] colliders; //Array of the collider GameObjects
+    public float screenPadding = 0.05f; //Padding around the screen
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Start()
+    { //Get the screen bounds in world space
+        Vector3 bottomLeft = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0));
+        Vector3 topRight = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, 0));
+
+        //Calculate the width and height of the screen
+        float width = topRight.x - bottomLeft.x;
+        float height = topRight.y - bottomLeft.y;
+
+        //Set the position and scale of each collider
+        colliders[0].transform.position = new Vector3(bottomLeft.x + width / 2, topRight.y + screenPadding, 0);
+        colliders[0].transform.localScale = new Vector3(width, screenPadding, 1);
+
+        colliders[1].transform.position = new Vector3(topRight.x + screenPadding, bottomLeft.y + height / 2, 0);
+        colliders[1].transform.localScale = new Vector3(screenPadding, height, 1);
+
+        colliders[2].transform.position = new Vector3(bottomLeft.x + width / 2, bottomLeft.y - screenPadding, 0);
+        colliders[2].transform.localScale = new Vector3(width, screenPadding, 1);
+
+        colliders[3].transform.position = new Vector3(bottomLeft.x - screenPadding, bottomLeft.y + height / 2, 0);
+        colliders[3].transform.localScale = new Vector3(screenPadding, height, 1);
     }
 }
